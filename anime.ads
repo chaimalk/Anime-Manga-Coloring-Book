f27579b4d8005239b73efa9dc2e765d7105781de
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-19 21:12:10 +0100

    Create anime.ads

---
+++ anime.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711074800",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2832584800",
+    "applovinBanner": "eac722631ec93fba",
+    "applovinInterstitial": "cf1676b197d86c97",
+    "unityGameId": "5172085",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
